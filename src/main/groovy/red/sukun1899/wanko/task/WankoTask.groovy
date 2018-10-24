4ef54ee58dea3b3eb029dfb644fdaefbbda6c2d6
su-kun1899 <higedrum.coz@gmail.com>
2018-10-24 21:25:03 +0900

    PostgreSQLを依存に追加

--- src/main/groovy/red/sukun1899/wanko/task/WankoTask.groovy
+++ src/main/groovy/red/sukun1899/wanko/task/WankoTask.groovy
@@ -1,5 +1,6 @@
 package red.sukun1899.wanko.task
 
+import groovy.sql.Sql
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -10,5 +11,14 @@ class WankoTask extends DefaultTask {
     @TaskAction
     def loadData() {
         println("Not implemented") // TODO
+        println("hoge") // TODO
+
+        // TODO オプションとして受け取るようにする
+        def sql = Sql.newInstance(
+                "jdbc:postgresql://localhost:25432/gengar-sayque",
+                "gengar",
+                "gengar",
+                "org.postgresql.Driver"
+        )
     }
 }
