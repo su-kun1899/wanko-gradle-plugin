ce7cd490bf73c6e29ba81b8a2296745a3abe7968
su-kun1899 <higedrum.coz@gmail.com>
2018-11-16 16:00:47 +0900

    テスト用のビルドファイルを使うようにする

--- src/test/groovy/red/sukun1899/wanko/WankoPluginBuildSpec.groovy
+++ src/test/groovy/red/sukun1899/wanko/WankoPluginBuildSpec.groovy
@@ -1,6 +1,6 @@
 package red.sukun1899.wanko
 
-import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import spock.lang.Specification
@@ -9,61 +9,15 @@ import spock.lang.Specification
  * @author su-kun1899
  */
 class WankoPluginBuildSpec extends Specification {
-    TemporaryFolder testProjectDir
-    File buildFile
-
-    def setup() {
-        testProjectDir = new TemporaryFolder()
-        testProjectDir.create()
-        buildFile = testProjectDir.newFile 'build.gradle'
-        buildFile << """
-            buildscript {
-                repositories {
-                    mavenCentral()
-                }
-
-                dependencies {
-                    classpath 'org.postgresql:postgresql:9.3-1100-jdbc41'
-                }
-            }
-                      
-            plugins {
-                id "red.sukun1899.wanko"
-            }
-            
-            repositories {
-                mavenCentral()
-            }            
-        """
-    }
-
-    def cleanup() {
-        testProjectDir.delete()
-    }
-
     def "Load task"() {
         given:
-        buildFile << """
-            wanko {
-                url = 'jdbc:postgresql://localhost:35432/postgres'
-                user = 'postgres'
-                password = 'wanko'
-                driverClassName = 'org.postgresql.Driver'
-                sqlDir = 'sql'
-            }
-        """
-
-        and:
-        testProjectDir.newFolder('sql')
-        def sql = testProjectDir.newFile('sql' + File.separator + 'hoge.sql')
-        sql << """
-            SELECT * FROM pg_catalog.pg_tables;
-        """
-
+        def testProjectDir = new File(
+                this.getClass().getResource('/buildspec/project').toURI()
+        )
 
         when:
         def result = GradleRunner.create()
-                .withProjectDir(testProjectDir.root)
+                .withProjectDir(testProjectDir)
                 .withArguments('wankoLoad')
                 .withPluginClasspath()
                 .withDebug(true)
